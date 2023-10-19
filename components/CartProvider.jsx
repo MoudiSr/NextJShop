2de5b4836b2533b57cbcad4386e1c476c1517f94
Mohammad Srouji <mhmmdsrouji@gmail.com>
2023-10-19 23:55:28 +0300

    update

--- components/CartProvider.jsx
+++ components/CartProvider.jsx
@@ -1,10 +1,14 @@
 'use client'
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 export const CartContext = React.createContext()
 
 const CartProvider = ({children}) => {
-    const [cart, setCart] = useState(JSON.parse(localStorage.getItem("cart") || "[]"))
+    const [cart, setCart] = useState()
+
+    useEffect(() => {
+        setCart(JSON.parse(localStorage.getItem("cart") || "[]"))
+    }, [])
 
     return (
         <CartContext.Provider value={{ cart, setCart }}>

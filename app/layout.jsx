293a3f68130c26738898d4ad3d94df61e2ec9dca
Mohammad Srouji <mhmmdsrouji@gmail.com>
2023-10-20 14:24:17 +0300

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import '@styles/global.css'
 import ShoppingCart from 'components/ShoppingCart'
 import {useState} from "react"
 import CartProvider from '@components/CartProvider'
+import { motion, AnimatePresence } from "framer-motion"
 
 const metadata = {
     title: "Store",
@@ -13,28 +14,37 @@ const RootLayout = ({ children }) => {
     const [open, setOpen] = useState(false)
 
     return (
+        <AnimatePresence>
         <html>
-            <CartProvider>
-            <body className='bg-gray-800'>
-                <nav className='w-full bg-[#444444] border-b-2 border-b-[#da0037] shadow-inner h-auto p-4'>
-                    <div className='flex justify-between'>
-                        <div className='space-x-4'>
-                            <a href='/'>Home</a>
-                            <a href='/shop'>Shop</a>
-                        </div>
-                        <div>
-                            <button onClick={() => setOpen(true)}>Cart</button>
-                            <ShoppingCart open={open} setOpen={setOpen}/>
-                        </div>
-                    </div>
-                </nav>
-                
-                <main className="app">
-                    { children }
-                </main>
-            </body>
-            </CartProvider>
+            <motion.div
+                initial="initialState"
+                animate="animateState"
+                exit="exitState"
+                className="base-page-size"
+            >
+                <CartProvider>
+                    <body className='bg-gray-800'>
+                        <nav className='w-full bg-[#444444] border-b-2 border-b-[#da0037] shadow-inner h-auto p-4'>
+                            <div className='flex justify-between'>
+                                <div className='space-x-4'>
+                                    <a href='/'>Home</a>
+                                    <a href='/shop'>Shop</a>
+                                </div>
+                                <div>
+                                    <button onClick={() => setOpen(true)}>Cart</button>
+                                    <ShoppingCart open={open} setOpen={setOpen}/>
+                                </div>
+                            </div>
+                        </nav>
+                        
+                        <main className="app">
+                            { children }
+                        </main>
+                    </body>
+                </CartProvider>
+            </motion.div>
         </html>
+        </AnimatePresence>
     )
 }
 
--- components/ShoppingCart.jsx
+++ components/ShoppingCart.jsx
@@ -3,6 +3,7 @@ import { Fragment, useEffect, useContext, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { CartContext } from "@components/CartProvider"
+import { motion } from "framer-motion"
 
 export default function ShoppingCart({open, setOpen}) {
     const {cart, setCart} = useContext(CartContext)
@@ -74,8 +75,16 @@ export default function ShoppingCart({open, setOpen}) {
                       <div className="mt-8">
                         <div className="flow-root">
                           <ul role="list" className="-my-6 divide-y divide-gray-200">
-                            {cart.map(product => (
-                              <li key={product.id} className="flex py-6">
+                            {cart.map((product, index) => (
+                              <motion.li 
+                                initial={{ opacity: 0, x: -100 }}
+                                whileInView={{ opacity: 1, x: 0, transition: {delay: 0.05*index} }}
+                                viewport={{
+                                  once: true,
+                                }}
+                                key={product.id} 
+                                className="flex py-6"
+                              >
                                 <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                                   <img
                                     src={product.image}
@@ -111,7 +120,7 @@ export default function ShoppingCart({open, setOpen}) {
                                     </div>
                                   </div>
                                 </div>
-                              </li>
+                              </motion.li>
                             ))}
                           </ul>
                         </div>

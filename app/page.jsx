115f0d53af086fdcacc655837ba8b59137651b3e
Mohammad Srouji <mhmmdsrouji@gmail.com>
2023-12-09 07:14:27 +0200

    update

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 
 const Home = () => {
     return (
-        <div className="flex p-4 justify-center align-center mt-[9rem]">
+        <div className="flex p-4 justify-center align-center mt-[2rem]">
             <div className='grid'>
                 <img src="assets/images/icon2xxxhdpi.png" className="w-60 h-60 m-2 mb-12" />
                 <motion.button className="rounded-xl shadow-xl bg-[#00B49F] p-2"
@@ -14,6 +14,9 @@ const Home = () => {
                 >
                     <Link href="/shop">Shop Now</Link>
                 </motion.button>
+                <div className="flex justify-center mt-10">
+                    <img src="assets/images/shi3ar.png" className="w-22 h-20" />
+                </div>
             </div>
         </div>
     )

aac18b436e84544fea3f7dfa8fc5efa676a92891
Mohammad Srouji <40596420+MoudiSr@users.noreply.github.com>
2024-01-04 08:46:15 +0000

    updatepreFinal

--- components/ShoppingCart.jsx
+++ components/ShoppingCart.jsx
@@ -138,14 +138,14 @@ export default function ShoppingCart({open, setOpen}) {
                         <p>${total}</p>
                       </div>
                       <div className="mt-6">
-                        <Link
-                          href="/checkout"
-                          className="flex items-center justify-center rounded-md border border-transparent bg-[#E4C048] px-6 py-3 text-base font-medium text-gray-900 shadow-sm hover:bg-[#E4C048]"
-                        >
-                          <button onClick={() => setOpen(false)}>
-                            Checkout
-                          </button>
-                        </Link>
+                        <button onClick={() => setOpen(false)} 
+                          className="flex items-center justify-center rounded-md border border-transparent bg-[#E4C048] px-6 py-3 text-base font-medium text-gray-900 shadow-sm hover:bg-[#E4C048] w-full">
+                          <Link
+                            href="/checkout"
+                          >
+                              Checkout
+                          </Link>
+                        </button>
                       </div>
                       <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                         <p>

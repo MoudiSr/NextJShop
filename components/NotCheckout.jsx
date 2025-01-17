544d80334d0b7840f130c7e50de0830faca624ee
Mohammad Srouji <40596420+MoudiSr@users.noreply.github.com>
2025-01-17 13:27:40 +0000

    Fix number of cart items to include the quantity of all products

--- components/NotCheckout.jsx
+++ components/NotCheckout.jsx
@@ -1,17 +1,25 @@
-import { useContext } from "react"
+import { useContext, useState, useEffect } from "react"
 import { CartContext } from "@components/CartProvider"
 import ShoppingCart from "@components/ShoppingCart"
 import { BsCart } from "react-icons/bs";
 
 const NotCheckout = ({open, setOpen}) => {
     const {cart, setCart} = useContext(CartContext)
+    const [totalQuantity, setTotalQuantity] = useState(0)
+
+    useEffect(() => {
+        setTotalQuantity(0)
+        cart.forEach(item => {
+            setTotalQuantity(prev => item.quantity+prev)
+        });
+    }, [cart])
 
     return (
         <>
             <div className="cart">
                 <button className="cartBtn" onClick={() => setOpen(true)}>
                     <BsCart className="text-xl text-[#E4C048]" />
-                    <span className="cartCount">{cart.length}</span>
+                    <span className="cartCount">{totalQuantity}</span>
                 </button>
             </div>
 

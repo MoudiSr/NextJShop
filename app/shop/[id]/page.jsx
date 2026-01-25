33ca0fa7f2b6947d3933e33305d8ff668c51b60d
Mhmmd Sr <mhmmdsrouji@gmail.com>
2026-01-25 16:08:23 +0200

    remove console.log

--- app/shop/[id]/page.jsx
+++ app/shop/[id]/page.jsx
@@ -7,7 +7,6 @@ import { redirect, usePathname } from "next/navigation"
 const Shop = () => {
     const products = data;
     const path = usePathname();
-    console.log(path)
 
     return (
         <div> 
